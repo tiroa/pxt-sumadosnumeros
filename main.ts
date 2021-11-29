291f3bf53dabfb5c3ec2ca0a1b4f9bb96d2aaccd
tobiasiroa <92176141+tobiasiroa@users.noreply.github.com>
2021-11-29 15:50:37 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * PCA9685
+ */
+//% weight=100 color=#0fbc11 icon="\uf1ec"
+namespace pruebasuma {
+    //% block
+    export function sumando(num1 : number, num2 : number) : number{
+        let resultado = num1 + num2;
+        return resultado;
+    }
+}
