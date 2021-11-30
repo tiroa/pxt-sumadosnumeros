9b8de153fde9b9f183c007d9034540c0a95944bd
tiroa <95303626+tiroa@users.noreply.github.com>
2021-11-30 14:12:29 -0300

    Add files via upload

---
+++ Ejemplos/pxt-sumadosnumeros/main.ts
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+//% weight=100 color=#0fbc11 icon="\uf1ec"
+namespace sumadosnumeros {
+    //% block
+    export function sumando(num1 : number, num2 : number) : number{
+        let resultado = num1 + num2;
+        return resultado;
+    }
+}
